d4c2ef4dc905f7476f358dfe375c4de555b25a78
Praharsh <praharsh075@gmail.com>
2026-03-30 18:29:28 +0530

    refactor: remove unused function from article listing utilities
    - Removed `extractTextFromContent` from imports in `articles.ts` as it is no longer needed, streamlining the codebase.

--- convex/articles.ts
+++ convex/articles.ts
@@ -10,7 +10,6 @@ import {
 } from './lib/articleSlug'
 import {
   buildSearchContent,
-  extractTextFromContent,
   removeTagLinksForArticle,
   replaceTagLinksForArticle,
 } from './lib/articleListing'
