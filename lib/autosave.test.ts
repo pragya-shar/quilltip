4956990cd37836935a418b1160095185cd55c69a
Praharsh Gothankar <praharsh075@gmail.com>
2026-05-19 22:40:41 +0530

    refactor(tests): simplify import statements in autosave test file

--- lib/autosave.test.ts
+++ lib/autosave.test.ts
@@ -1,8 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import {
-  AUTO_SAVE_DEBOUNCE_SECONDS,
-  AUTO_SAVE_GUIDANCE,
-} from './autosave'
+import { AUTO_SAVE_DEBOUNCE_SECONDS, AUTO_SAVE_GUIDANCE } from './autosave'
 
 describe('autosave copy', () => {
   it('guidance includes debounce seconds from AUTO_SAVE_DEBOUNCE_MS', () => {
